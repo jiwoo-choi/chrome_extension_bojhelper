0fa4b4914f484367303be11dbaccecd887f71ffe
Jiwoo Choi <pelomon2@gmail.com>
2020-06-13 18:57:06 -0500

    # 버전2 ## popup.js - popup.js 스타일 내용 변경. ## content.js - 문제보기 스타일 변경

--- content.js
+++ content.js
@@ -1,68 +1,146 @@
+const test = document.createElement('li');
+test.innerHTML = `<a id="myBtn">문제보기</a>`
+document.querySelector('.nav.nav-pills.no-print.problem-menu').appendChild(test);
 
-document.querySelector('legend').innerHTML = document.querySelector('legend').innerText + `
-<a id="myBtn">문제보기</a>
-<!-- The Modal -->
-<div id="myModal" class="modal">
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <div id="title"></div>
-    <div id="border"></div>
-    <div id="border1">
-    <div id="result"></div>
-    </div>
-    <br>
-    <div id="subtitle">입력</div>
-    <div id="border"></div>
-    <div id="border1">
-    <div id="input"></div>
-    </div>
-    <br>
-    <div id="subtitle">출력</div>
-    <div id="border"></div>
-    <div id="border1">
-    <div id="output"></div>
-    </div>
-    <br>
-    <div id="subtitle">입력예시</div>
-    <div id="border"></div>
-    <div id="border1">
-    <div id="tt"></div>
-    </div>
-  </div>
+const modal = document.createElement('div')
+modal.setAttribute('id', 'myModal');
+modal.classList.add('modal');
+modal.innerHTML =
+`
+<div class="modal-content">
+<span class="close">&times;</span>
+<div id="title"></div>
+<div id="border"></div>
+<div id="border1">
+<div id="result"></div>
+</div>
+<br>
+<div id="subtitle">입력</div>
+<div id="border"></div>
+<div id="border1">
+<div id="input"></div>
+</div>
+<br>
+<div id="subtitle">출력</div>
+<div id="border"></div>
+<div id="border1">
+<div id="output"></div>
+</div>
+<br>
+<div id="subtitle">입력예시</div>
+<div id="border"></div>
+<div id="border1">
+<div id="tt"></div>
+</div>
+</div>
 </div>
 `
+document.body.appendChild(modal);
 
-
-
-var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+test.onclick = function() {
+  modal.style.display = 'block';
 }
 
-// When the user clicks on <span> (x), close the modal
+var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = "none";
   }
 }
 
 
+// var modal = document.getElementById("myModal");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+
 async function load(parser){
+
     let urls = location.href.split('/');
     await parser("https://www.acmicpc.net/problem/"+urls[4]);
+
+
+    const chosen = document.querySelector('.chosen-single span');
+
+    if(urls.length > 4 && !chosen.innerText.includes("C++")) {
+      return;
+    }
+    
+
+    let cppheaders = [];
+    let usingnamespaceflag = false;
+    let intmainflag = false;
+
+    chrome.storage.local.get(['cppheaders'], function(result) {
+      cppheaders = cppheaders = result.cppheaders ? result.cppheaders : cppheaders;
+      let code = cppheaders.reduce( (prev, curr) => {
+        if (curr === "using namespace std") {
+          usingnamespaceflag = true;
+          return prev;
+        }
+
+        if (curr === "int main") {
+          intmainflag = true;
+          return prev;
+        }
+
+        prev += `#include <${curr}> \\n` 
+        return prev
+      }, "");
+
+      
+      if (usingnamespaceflag) {
+        code += `using namespace std; \\n`
+      }
+
+      if (intmainflag) {
+        code += `int main() {  \\n`
+        code += `} \\n`
+      }
+
+
+      let actualCode = `
+      const mirror = OnlineJudgeCodeMirror.get("source"); \n       
+      mirror.replaceRange("${code}",mirror.getCursor())
+      `
+
+      console.log(actualCode);
+
+      var script = document.createElement('script');
+      script.textContent = actualCode;
+      (document.head||document.documentElement).appendChild(script);
+      script.remove();
+  
+    });  
+
+
+    //https://stackoverflow.com/questions/9515704/insert-code-into-the-page-context-using-a-content-script
+
+    
 }
 
 
--- popup.js
+++ popup.js
@@ -2,21 +2,138 @@
 // if it's url is "boj/submit/"
 // then load something!
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems);
+// });
 
 
-/** when loaded */
-function load(parser){
 
-    chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
-        if (tabs[0].url.includes("https://www.acmicpc.net/submit")){
-            let urls = tabs[0].url.split('/');
-            await parser("https://www.boj.kr/"+urls[4]);
-            // await parser("https://www.acmicpc.net/problem/1172");
+function hideLayer(){
+    const layer = document.querySelector(".layer");
+    layer.style.display = 'none'
+}
+
+function goto(){
+
+    // chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
+    //     chrome.tabs.executeScript(tabs[0].id, {}, ()=>{
+    //         window.location.href = "https://www.acmicpc.net/"
+    //     })
+    // })
+    window.open("https://www.acmicpc.net/");
+}
+
+
+function gotoNextPage(){
+    window.scrollTo({
+        left: 600,
+        behavior: 'smooth'
+      });
+}
+
+function back(){
+
+    window.scrollTo({
+        left: 0,
+        behavior: 'smooth'
+      });
+}
+
+function load(){
+
+    document.querySelector("#hide-layer").onclick = function() {
+        hideLayer();
+    }
+
+    document.querySelector("#goto").onclick = function() {
+        goto();
+    }
+
+    document.querySelector("#go-next").onclick = function() {
+        gotoNextPage();
+    }
+    document.querySelector("#go-back").onclick = function() {
+        back();
+    }
+
+    document.querySelector(".sub-menu-container .chip-container").addEventListener('click',
+        (event) => {
+            const {target: { tagName, innerText }} = event;
+            if (tagName === "SPAN") {
+                window.open('http://boj.kr/' + innerText)
+            }
         }
+    )
+
+
+    let cppheaders = [];
+
+    chrome.storage.local.get(['cppheaders'], function(result) {
+        cppheaders = result.cppheaders ? result.cppheaders : cppheaders;
+        const nodelist = document.querySelectorAll(".main-menu-container .card .chip-container span")
+        
+        for (let node of nodelist) {
+            if (cppheaders.includes(node.innerText)) {
+                node.classList.add('active');
+            }
+        }
+    });  
+
+    document.querySelector("#cpp-header-save-button").onclick = function(){
+
+
+        cppheaders = [];
+        const nodelist = document.querySelectorAll(".main-menu-container .card .chip-container span");
+        for (let node of nodelist) {
+            if (node.classList.contains("active")) {
+                cppheaders.push(node.innerText)
+            }
+        }
+
+        
+        cppheaders = cppheaders.filter((item, index) => cppheaders.indexOf(item) === index)
+        chrome.storage.local.set({"cppheaders": cppheaders}, function() {
+            document.querySelector("#cpp-header-save-button").classList.add("success")
+            document.querySelector("#cpp-header-save-button").innerText = "저장완료!"
+            setTimeout( function () { 
+                document.querySelector("#cpp-header-save-button").classList.remove("success")
+                document.querySelector("#cpp-header-save-button").innerText = "저장하기"
+            }, 1000);
+        });
+                 
+    }
+
+    document.querySelector(".main-menu-container .card .chip-container").addEventListener('click',
+        (event) => {
+            const {target: { tagName, innerText }} = event;
+            if (tagName === "SPAN") {
+                if (event.target.classList.contains("active")) {
+                    event.target.classList.remove("active");
+                } else {
+                    event.target.classList.add("active");
+                }
+            }
+        }
+    )
+    
+    chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
+
+        const url = new URL(tabs[0].url);
+        if (url.hostname === "www.acmicpc.net"){
+            const layer = document.querySelector(".layer");
+            layer.style.display = 'none'
+        } else {
+            
+        }
+
+        
+
+        // if (tabs[0].url.includes("https://www.acmicpc.net/")){
+        //     let urls = tabs[0].url.split('/');
+        //     await parser("https://www.boj.kr/"+urls[4]);
+        //     // await parser("https://www.acmicpc.net/problem/1172");
+        // }
     })
 
     return false;
@@ -35,11 +152,6 @@ async function parser(url){
         value.src = "https://www.acmicpc.net/upload/"+ value.src.split('/upload/')[1];
     })
 
-    var text = doc.querySelector('#problem_description').innerHTML
-    var title = doc.querySelector('#problem_title').innerText
-    var input = doc.querySelector('#problem_input').innerText
-    var output = doc.querySelector('#problem_output').innerText
-    var ts = doc.querySelectorAll('.sampledata');
     
 
     let index = 0;
